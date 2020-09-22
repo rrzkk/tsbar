c85900c077cacbf33fa697fc86c683e69f619fdc
ren <rrzkk@live.com>
2020-09-22 23:14:26 +1000

    test front end

--- reactui/src/test/LoginForm.test.tsx
+++ reactui/src/test/LoginForm.test.tsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { render, fireEvent, waitForElement } from "@testing-library/react";
+import { render} from "@testing-library/react";
 import App from "../App";
 
 describe("App simple test", () => {
